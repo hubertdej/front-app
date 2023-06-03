b6bbfe69277deaa0d0c1bdf4f8e32c4309463174
Maciej Matys <m.j.matys@gmail.com>
2023-06-03 20:25:28 +0200

    reformat

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -31,73 +31,73 @@ function DashboardContent( props:{ dashboardId:string } ) {
   const [splitPanelSize, setSplitPanelSize] = useState(splitPanelMaxSize);
 
   return (
-          <AppLayout
-              contentType="dashboard"
-              breadcrumbs={<BreadcrumbGroup
-                  items={[
-                    { text: 'App', href: '#' },
-                    { text: 'Dashboard', href: '#/' },
-                  ]}
-                  expandAriaLabel="Show path"
-                  ariaLabel="Breadcrumbs"
-              />}
-              navigationHide={true}
-              toolsHide={true}
-              content={
-                  <ContentLayout
-                      header={
-                          <Header
-                              variant="h1"
-                              actions={
-                                  <SpaceBetween size="xs" direction="horizontal">
-                                      <Button iconName="add-plus" onClick={() => setSplitPanelOpen(true)}>
-                                          Add widget
-                                      </Button>
-                                  </SpaceBetween>
-                              }
-                          >
-                              Dashboard {dashboardId}
-                          </Header>
-                      }>
-                      <Board
-                          empty={
-                              <EmptyState
-                                  title="No widgets"
-                                  description="There are no widgets on the dashboard."
-                                  verticalCenter={true}
-                                  action={
-                                      <Button iconName="add-plus" onClick={() => setSplitPanelOpen(true)}>
-                                          Add widget
-                                      </Button>
-                                  }
-                              />
+      <AppLayout
+          contentType="dashboard"
+          breadcrumbs={<BreadcrumbGroup
+              items={[
+                { text: 'App', href: '#' },
+                { text: 'Dashboard', href: '#/' },
+              ]}
+              expandAriaLabel="Show path"
+              ariaLabel="Breadcrumbs"
+          />}
+          navigationHide={true}
+          toolsHide={true}
+          content={
+              <ContentLayout
+                  header={
+                      <Header
+                          variant="h1"
+                          actions={
+                              <SpaceBetween size="xs" direction="horizontal">
+                                  <Button iconName="add-plus" onClick={() => setSplitPanelOpen(true)}>
+                                      Add widget
+                                  </Button>
+                              </SpaceBetween>
                           }
-                          i18nStrings={boardI18nStrings}
-                          items={getBoardWidgets(layout)}
-                          onItemsChange={({ detail }) => {
-                            setLayout(exportLayout(detail.items));
-                          }}
-                          renderItem={(item, actions) => (
-                              <ConfigurableWidget config={item.data} onRemove={actions.removeItem} />
-                          )}
-                      />
-                  </ContentLayout>
-              }
-              splitPanel={
-                  <SplitPanel
-                      header="Add widgets"
-                      closeBehavior="hide"
-                      hidePreferencesButton={true}
-                      i18nStrings={splitPanelI18nStrings}>
-                      <Palette items={getPaletteWidgets(layout)} />
-                  </SplitPanel>
-              }
-              splitPanelPreferences={{ position: 'side' }}
-              splitPanelOpen={splitPanelOpen}
-              onSplitPanelToggle={({ detail }) => setSplitPanelOpen(detail.open)}
-              splitPanelSize={splitPanelSize}
-              onSplitPanelResize={event => setSplitPanelSize(Math.min(event.detail.size, splitPanelMaxSize))}
-          />
+                      >
+                          Dashboard {dashboardId}
+                      </Header>
+                  }>
+                  <Board
+                      empty={
+                          <EmptyState
+                              title="No widgets"
+                              description="There are no widgets on the dashboard."
+                              verticalCenter={true}
+                              action={
+                                  <Button iconName="add-plus" onClick={() => setSplitPanelOpen(true)}>
+                                      Add widget
+                                  </Button>
+                              }
+                          />
+                      }
+                      i18nStrings={boardI18nStrings}
+                      items={getBoardWidgets(layout)}
+                      onItemsChange={({ detail }) => {
+                        setLayout(exportLayout(detail.items));
+                      }}
+                      renderItem={(item, actions) => (
+                          <ConfigurableWidget config={item.data} onRemove={actions.removeItem} />
+                      )}
+                  />
+              </ContentLayout>
+          }
+          splitPanel={
+              <SplitPanel
+                  header="Add widgets"
+                  closeBehavior="hide"
+                  hidePreferencesButton={true}
+                  i18nStrings={splitPanelI18nStrings}>
+                  <Palette items={getPaletteWidgets(layout)} />
+              </SplitPanel>
+          }
+          splitPanelPreferences={{ position: 'side' }}
+          splitPanelOpen={splitPanelOpen}
+          onSplitPanelToggle={({ detail }) => setSplitPanelOpen(detail.open)}
+          splitPanelSize={splitPanelSize}
+          onSplitPanelResize={event => setSplitPanelSize(Math.min(event.detail.size, splitPanelMaxSize))}
+      />
   );
 }
 
