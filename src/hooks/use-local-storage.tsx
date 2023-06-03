275f1b893617d5611b2f6325f0c97f0602a6b81e
Maciej Matys <m.j.matys@gmail.com>
2023-06-03 17:01:54 +0200

    ensure dashboards render separately

--- src/hooks/use-local-storage.tsx
+++ src/hooks/use-local-storage.tsx
@@ -1,28 +1,25 @@
 import { useState, useEffect, Dispatch, SetStateAction } from 'react';
 
 
-function useLocalStorage<S>(key: string, initialState: S, serialize: (value: S) => string, deserialize: (serialized: string) => S): [S, Dispatch<SetStateAction<S>>] {
+function useLocalStorage<S>(key: string, initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>] {
   const [value, setValue] = useState<S>(() => {
     const storedValue = localStorage.getItem(key);
-  
+
     if (storedValue) {
-      return deserialize(storedValue);
+      return JSON.parse(storedValue);
     }
-  
+
     if (typeof initialState === 'function') {
-      const initialValue = (initialState as () => S)();
-      localStorage.setItem(key, serialize(initialValue));
-      return initialValue;
+      return (initialState as () => S)();
     }
-  
-    localStorage.setItem(key, serialize(initialState));
+
     return initialState;
   });
-  
+
   useEffect(() => {
-    localStorage.setItem(key, serialize(value));
-  }, [key, value, serialize]);
-  
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
   return [value, setValue];
 }
 
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -15,105 +15,100 @@ import Palette from './components/palette';
 import { ConfigurableWidget } from './components/configurable-widget';
 import { exportLayout, getBoardWidgets, getDefaultLayout, getPaletteWidgets } from './widgets';
 import { useLoaderData, LoaderFunctionArgs } from 'react-router-dom';
-import { WidgetPlacement } from './widgets/interfaces';
 import useLocalStorage from '../../hooks/use-local-storage';
 
 const splitPanelMaxSize = 360;
-
-type Params = {
-  dashboardId:string
-};
 
 export async function loader({ params } : LoaderFunctionArgs) {
   const dashboardId = params.dashboardId;
   return { dashboardId };
 }
 
-const defaultLayoutStore = new Map<string, ReadonlyArray<WidgetPlacement>>([
-  ['1', getDefaultLayout()],
-  ['2', getDefaultLayout()],
-  ['3', getDefaultLayout()],
-]);
-
-function Dashboard() {
-  const { dashboardId } = useLoaderData() as Params;
-  const [layoutStore, setLayoutStore] = useLocalStorage<Map<string, ReadonlyArray<WidgetPlacement>>>(
-    'dashboardLayoutStore',
-    defaultLayoutStore,
-    (map: Map<string, ReadonlyArray<WidgetPlacement>>) => JSON.stringify(Array.from(map.entries())),
-    (json: string) => new Map<string, ReadonlyArray<WidgetPlacement>>(JSON.parse(json)));
+function DashboardContent( props:{ dashboardId:string } ) {
+  const dashboardId = props.dashboardId;
+  const [layout, setLayout] = useLocalStorage(dashboardId, getDefaultLayout);
   const [splitPanelOpen, setSplitPanelOpen] = useState(false);
   const [splitPanelSize, setSplitPanelSize] = useState(splitPanelMaxSize);
 
   return (
-        <AppLayout
-            contentType="dashboard"
-            breadcrumbs={<BreadcrumbGroup
-                items={[
-                  { text: 'App', href: '#' },
-                  { text: 'Dashboard', href: '#/' },
-                ]}
-                expandAriaLabel="Show path"
-                ariaLabel="Breadcrumbs"
-            />}
-            navigationHide={true}
-            toolsHide={true}
-            content={
-                <ContentLayout
-                    header={
-                        <Header
-                            variant="h1"
-                            actions={
-                                <SpaceBetween size="xs" direction="horizontal">
-                                    <Button iconName="add-plus" onClick={() => setSplitPanelOpen(true)}>
-                                        Add widget
-                                    </Button>
-                                </SpaceBetween>
-                            }
-                        >
-                            Dashboard {dashboardId}
-                        </Header>
-                    }>
-                    <Board
-                        empty={
-                            <EmptyState
-                                title="No widgets"
-                                description="There are no widgets on the dashboard."
-                                verticalCenter={true}
-                                action={
-                                    <Button iconName="add-plus" onClick={() => setSplitPanelOpen(true)}>
-                                        Add widget
-                                    </Button>
-                                }
-                            />
-                        }
-                        i18nStrings={boardI18nStrings}
-                        items={getBoardWidgets(layoutStore.get(dashboardId) || getDefaultLayout())}
-                        onItemsChange={({ detail }) => {
-                          setLayoutStore(new Map(layoutStore).set(dashboardId, exportLayout(detail.items)));
-                        }}
-                        renderItem={(item, actions) => (
-                          <ConfigurableWidget config={item.data} onRemove={actions.removeItem} />
-                        )}
-                    />
-                </ContentLayout>
-            }
-            splitPanel={
-                <SplitPanel
-                    header="Add widgets"
-                    closeBehavior="hide"
-                    hidePreferencesButton={true}
-                    i18nStrings={splitPanelI18nStrings}>
-                  <Palette items={getPaletteWidgets(layoutStore.get(dashboardId) || getDefaultLayout())} />
-                </SplitPanel>
-            }
-            splitPanelPreferences={{ position: 'side' }}
-            splitPanelOpen={splitPanelOpen}
-            onSplitPanelToggle={({ detail }) => setSplitPanelOpen(detail.open)}
-            splitPanelSize={splitPanelSize}
-            onSplitPanelResize={event => setSplitPanelSize(Math.min(event.detail.size, splitPanelMaxSize))}
-        />
+          <AppLayout
+              contentType="dashboard"
+              breadcrumbs={<BreadcrumbGroup
+                  items={[
+                    { text: 'App', href: '#' },
+                    { text: 'Dashboard', href: '#/' },
+                  ]}
+                  expandAriaLabel="Show path"
+                  ariaLabel="Breadcrumbs"
+              />}
+              navigationHide={true}
+              toolsHide={true}
+              content={
+                  <ContentLayout
+                      header={
+                          <Header
+                              variant="h1"
+                              actions={
+                                  <SpaceBetween size="xs" direction="horizontal">
+                                      <Button iconName="add-plus" onClick={() => setSplitPanelOpen(true)}>
+                                          Add widget
+                                      </Button>
+                                  </SpaceBetween>
+                              }
+                          >
+                              Dashboard {dashboardId}
+                          </Header>
+                      }>
+                      <Board
+                          empty={
+                              <EmptyState
+                                  title="No widgets"
+                                  description="There are no widgets on the dashboard."
+                                  verticalCenter={true}
+                                  action={
+                                      <Button iconName="add-plus" onClick={() => setSplitPanelOpen(true)}>
+                                          Add widget
+                                      </Button>
+                                  }
+                              />
+                          }
+                          i18nStrings={boardI18nStrings}
+                          items={getBoardWidgets(layout)}
+                          onItemsChange={({ detail }) => {
+                            setLayout(exportLayout(detail.items));
+                          }}
+                          renderItem={(item, actions) => (
+                              <ConfigurableWidget config={item.data} onRemove={actions.removeItem} />
+                          )}
+                      />
+                  </ContentLayout>
+              }
+              splitPanel={
+                  <SplitPanel
+                      header="Add widgets"
+                      closeBehavior="hide"
+                      hidePreferencesButton={true}
+                      i18nStrings={splitPanelI18nStrings}>
+                      <Palette items={getPaletteWidgets(layout)} />
+                  </SplitPanel>
+              }
+              splitPanelPreferences={{ position: 'side' }}
+              splitPanelOpen={splitPanelOpen}
+              onSplitPanelToggle={({ detail }) => setSplitPanelOpen(detail.open)}
+              splitPanelSize={splitPanelSize}
+              onSplitPanelResize={event => setSplitPanelSize(Math.min(event.detail.size, splitPanelMaxSize))}
+          />
   );
 }
+
+function Dashboard() {
+  const { dashboardId } = useLoaderData() as { dashboardId:string };
+  return (
+        <div key={dashboardId}>
+            <DashboardContent  dashboardId={dashboardId}/>
+        </div>
+  );
+}
+
 
 export default Dashboard;
