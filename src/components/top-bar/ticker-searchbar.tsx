e164bb95e4469446ead7ad5fd3ccda9012af697e
Hubert Dej <34969251+hubertdej@users.noreply.github.com>
2023-06-16 18:04:32 +0200

    Update searchbar fetching

--- src/components/top-bar/ticker-searchbar.tsx
+++ src/components/top-bar/ticker-searchbar.tsx
@@ -1,34 +1,30 @@
 import { Autosuggest, AutosuggestProps } from '@cloudscape-design/components';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { fetchTickers } from '../../api/fetch-tickers';
-import { useDebounce } from '../../hooks/use-debounce';
 
 function TickerSearchbar() {
   const [query, setQuery] = useState('');
-  const debouncedQuery = useDebounce(query, 300);
   const [options, setOptions] = useState<AutosuggestProps.Options>([]);
   const navigate = useNavigate();
-
-  useEffect(() => {
-    fetchTickers(debouncedQuery).then(resultsByCategories => {
-      setOptions(
-        Object.entries(resultsByCategories).map<AutosuggestProps.OptionGroup>(([category, results]) => ({
-          label: category,
-          options: results.map<AutosuggestProps.Option>(({ symbol, name }) => ({
-            label: name,
-            value: symbol,
-            tags: [symbol],
-          })),
-        })),
-      );
-    });
-  }, [debouncedQuery]);
 
   return (
     <Autosuggest
       onChange={({ detail }) => setQuery(detail.value)}
       onSelect={({ detail }) => navigate(`/stock/${detail.value}`)}
+      onLoadItems={async ({ detail }) => {
+        const resultsByCategories = await fetchTickers(detail.filteringText);
+        setOptions(
+          Object.entries(resultsByCategories).map<AutosuggestProps.OptionGroup>(([category, results]) => ({
+            label: category,
+            options: results.map<AutosuggestProps.Option>(({ symbol, name }) => ({
+              label: name,
+              value: symbol,
+              tags: [symbol],
+            })),
+          })),
+        );
+      }}
       value={query}
       options={options}
       enteredTextLabel={value => `Use: ${value}`}
